88a446db4b108b79d3a56ec8c7b879da596bc277
Peter Doyle <peter@probook.wdventures.com>
2015-06-15 21:07:38 -0600

    added a node.js twitter client app

---
+++ node/twitter/twitter_streaming_client.js
@@ -0,0 +1,36 @@
+
+var Twitter = require('twitter-node-client').Twitter;
+var format = require('format-json-stream');
+
+//Callback functions
+var error = function (err, response, body) {
+    console.log('ERROR [%s]', err);
+};
+var success = function (data) {
+    console.log('Data [%s]', data);
+};
+
+
+//Get this data from your twitter apps dashboard
+var config = {
+    "consumerKey": "toLroxf9t7WzsdQvVSFVQr23y",
+    "consumerSecret": "WKYo5wjI2yGDqr0SmJyI5kUPGh5oIA4die6gmRYw4BZWIpFh2f",
+    "accessToken": "1665922212-bvs5aSBK5VFMyf6wzNskIWlWQlleqZVoTYO03Dd",
+    "accessTokenSecret": "zI0C8sOo2VzsCDitEGBFMgaM1bPGagJX5rHfNo8ngyCnv"
+}
+
+var twitter = new Twitter(config);
+
+
+//
+// Get 10 tweets containing the hashtag haiku
+//
+// print process.argv
+
+
+var myArgs = process.argv.slice(2);
+console.log('myArgs: ', myArgs);
+var hashtag = myArgs[0];
+var limit = myArgs[1];
+
+twitter.getSearch({'q':hashtag,'count': limit}, error, success);

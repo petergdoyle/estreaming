fc00401edb2ca658bf4a499362a85ed219266114
Peter Doyle <peter@engine1.cleverfishsoftware.com>
2015-07-06 09:58:57 -0400

    modifications to allow for CORS from the spring-xd admin app

---
+++ vagrant/open_host_ports.sh
@@ -0,0 +1,5 @@
+sudo firewall-cmd --zone=public --add-port=3000/tcp
+sudo firewall-cmd --zone=public --add-port=9393/tcp
+sudo firewall-cmd --zone=public --add-port=9889/tcp
+sudo firewall-cmd --zone=public --add-port=61616/tcp
+sudo firewall-cmd --zone=public --add-port=8080/tcp

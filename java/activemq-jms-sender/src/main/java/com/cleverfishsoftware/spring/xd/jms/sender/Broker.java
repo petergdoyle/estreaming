9cbd2ee5dadf59fed7005f44a36a6b25eb4e7b62
petergdoyle <peter.g.doyle@gmail.com>
2015-08-22 07:46:40 -0600

    set limits on activemq disk usage

--- java/activemq-jms-sender/src/main/java/com/cleverfishsoftware/spring/xd/jms/sender/Broker.java
+++ java/activemq-jms-sender/src/main/java/com/cleverfishsoftware/spring/xd/jms/sender/Broker.java
@@ -3,6 +3,7 @@
 package com.cleverfishsoftware.spring.xd.jms.sender;
 
 import org.apache.activemq.broker.BrokerService;
+import org.apache.activemq.usage.SystemUsage;
 
 /**
  *
@@ -11,25 +12,38 @@ import org.apache.activemq.broker.BrokerService;
 public class Broker {
 
     public static final String DEFAULT_BROKER_URL = "tcp://localhost:61616";
-    private static final String DEFAULT_BROKER_NAME = "broker";
+    public static final String DEFAULT_BROKER_NAME = "default";
+    private static final int MB = 1024 * 1024;
+    public static final int DEFAULT_STORE_USAGE = MB * 64;
+    public static final int DEFAULT_TMP_USAGE = MB * 64;
 
-    public static Broker create(String brokerName, String brokerUrl) {
-        return new Broker(brokerName, brokerUrl);
+    public static Broker create(String brokerName, String brokerUrl, int storeUsage, int tempUsage) {
+        return new Broker(brokerName, brokerUrl, storeUsage, tempUsage);
     }
 
     private final String brokerUrl;
     private final String brokerName;
+    private final int tempUsage;
+    private final int storeUsage;
 
-    private Broker(String brokerName, String brokerUrl) {
-        this.brokerName = brokerName;
-        this.brokerUrl = brokerUrl;
+    private Broker(String brokerName, String brokerUrl, int storeUsage, int tempUsage) {
+        this.brokerName = brokerName != null & brokerName.length() != 0 ? brokerName : DEFAULT_BROKER_NAME;
+        this.brokerUrl = brokerUrl != null & brokerUrl.length() != 0 ? brokerUrl : DEFAULT_BROKER_URL;
+        this.storeUsage = storeUsage > 0 ? storeUsage : DEFAULT_STORE_USAGE;
+        this.tempUsage = tempUsage > 0 ? tempUsage : DEFAULT_TMP_USAGE;
     }
 
     public void start() throws Exception {
         BrokerService broker = new BrokerService();
         broker.setBrokerName(brokerName);
         broker.addConnector(brokerUrl);
-        broker.start();
+
+        SystemUsage systemUsage = broker.getSystemUsage();
+        systemUsage.getStoreUsage().setLimit(storeUsage);
+        systemUsage.getTempUsage().setLimit(tempUsage);
+
+        broker.setPersistent(false);
+
         System.out.println("Started ActiveMQ broker: " + broker.toString());
     }
 
--- java/activemq-jms-sender/src/main/java/com/cleverfishsoftware/spring/xd/jms/sender/RunBroker.java
+++ java/activemq-jms-sender/src/main/java/com/cleverfishsoftware/spring/xd/jms/sender/RunBroker.java
@@ -2,8 +2,7 @@
  */
 package com.cleverfishsoftware.spring.xd.jms.sender;
 
-import static com.cleverfishsoftware.spring.xd.jms.sender.Broker.DEFAULT_BROKER_URL;
-import static org.apache.activemq.broker.BrokerService.DEFAULT_BROKER_NAME;
+import static com.cleverfishsoftware.spring.xd.jms.sender.Broker.*;
 
 /**
  *
@@ -16,7 +15,9 @@ public class RunBroker {
         try {
             Broker.create(
                     DEFAULT_BROKER_NAME,
-                    DEFAULT_BROKER_URL)
+                    DEFAULT_BROKER_URL,
+                    DEFAULT_STORE_USAGE,
+                    DEFAULT_TMP_USAGE)
                     .start();
         } catch (Exception ex) {
             System.out.println("Could not start broker: " + ex.getMessage());

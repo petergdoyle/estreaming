77bfcef619e27f2f954c11e972037436a31f1b9b
petergdoyle <peter.g.doyle@gmail.com>
2015-08-25 03:38:04 +0000

    added forgotten field aggregate counter tap to analytics_streams

--- xd-streams/demo_analytics_streams.cmd
+++ xd-streams/demo_analytics_streams.cmd
@@ -4,3 +4,5 @@ stream create airshop_stream_flight_type_tap  --definition "tap:stream:airshop_s
 stream create airshop_stream_flight_destination_city_count_tap --definition "tap:stream:airshop_stream.transform > field-value-counter --fieldName=flight.airportCityArr --name='Destination City'" --deploy
 
 stream create airshop_stream_flight_departure_city_count_tap --definition "tap:stream:airshop_stream.transform > field-value-counter --fieldName=flight.airportCityDpt --name='Departure City'" --deploy
+
+stream create airshop_stream_flight_count_tap --definition "tap:stream:airshop_stream.transform > aggregate-counter --name='Flight Data'" --deploy

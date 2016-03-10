d7adb45c93a9ab52c2696b1b5948d63db630db28
petergdoyle <peter.g.doyle@gmail.com>
2016-03-10 11:18:14 -0700

    changes to activemq start command

--- activemq-server/docker_run.sh
+++ activemq-server/docker_run.sh
@@ -2,9 +2,25 @@
 . ../scripts/lib/docker_functions.sh
 . ../scripts/lib/network_functions.sh
 
+start_cmd_min="java -Xms1G -Xmx1G \
+-jar /activemq/default/bin/activemq.jar start"
+
+start_cmd_max="java -Xms1G -Xmx1G \
+-Djava.util.logging.config.file=logging.properties \
+-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote \
+-Djava.io.tmpdir=/activemq/default/tmp \
+-Dactivemq.classpath=/activemq/default/conf \
+-Dactivemq.home=/activemq/default \
+-Dactivemq.base=/activemq/default \
+-Dactivemq.conf=/activemq/default/conf \
+-Dactivemq.data=/activemq/default/data \
+-jar /activemq/default/bin/activemq.jar start"
+
 img_name='estreaming/activemq-server'
 container_name='estreaming_activemq_server'
-start_cmd="bin/activemq start"
+
+start_cmd=$start_cmd_min
+
 network_port_mapped="-p 0.0.0.0:61616:61616 \
 -h $container_name.dkr"
 network="$network_port_mapped"

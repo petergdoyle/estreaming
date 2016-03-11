619d7fc30bd6f0786ba15803a536ec25153f4147
petergdoyle <peter.g.doyle@gmail.com>
2016-03-10 19:48:27 -0700

    allow for multiple jms message sender instances

--- activemq/docker_run_message_sender.sh
+++ activemq/docker_run_message_sender.sh
@@ -6,7 +6,12 @@
 img_name='estreaming/activemq'
 read -e -p "Enter the number of messages to generate (per second): " -i "10" message_rate
 read -e -p "Enter the activmq queue name: " -i "airshop" queue_name
-container_name='estreaming_activemq_message_sender'
-start_cmd="java -cp .:./spring-xd-jms-sender-1.0-SNAPSHOT.jar com.cleverfishsoftware.spring.xd.jms.sender.RunMessageSenderArgs $message_rate $queue_name"
-network="$network_native"
-docker_run
+read -e -p "Enter the number of instances to run: " -i "1" instances
+fi
+
+for i in $(eval echo "{1..$instances"});   do
+  container_name='estreaming_activemq_message_sender'_$i
+  start_cmd="java -cp .:./spring-xd-jms-sender-1.0-SNAPSHOT.jar com.cleverfishsoftware.spring.xd.jms.sender.RunMessageSenderArgs $message_rate $queue_name"
+  network="$network_native"
+  docker_run
+done

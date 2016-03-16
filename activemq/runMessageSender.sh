bad326af9966a85694ef7e230ed503f80f7d4daf
petergdoyle <peter.g.doyle@gmail.com>
2016-03-16 04:37:52 -0600

    modfied runMessageSender script to run the new MessageSender

--- activemq/runMessageSender.sh
+++ activemq/runMessageSender.sh
@@ -6,11 +6,28 @@ script_path=$( cd $(dirname $0) ; pwd -P )
 read -e -p "Enter the broker url : " -i "tcp://localhost:61616" broker_url
 read -e -p "Enter the activmq queue name: " -i "airshop" queue_name
 read -e -p "Enter the number of messages to generate (per second): " -i "10" message_rate
+read -e -p "Enter the max number of message (0 for unlimited): " -i "0" message_limit
+number_of_cores=$(grep -c ^processor /proc/cpuinfo)
+read -e -p "Enter number of threads (1-$number_of_cores): " -i "$number_of_cores" number_of_threads
+read -e -p "Enter the JMS Connection Factory Provider Class: " -i "com.cleverfishsoftware.spring.xd.jms.sender.sample.ActiveMQConnectionFactoryProvider" connectionFactoryClassName
 read -e -p "Display output to console (y/n) " -i "n" console_output
 
-java -cp .:$script_path/target/spring-xd-jms-sender-1.0-SNAPSHOT.jar \
+old_cmd="java -cp .:$script_path/target/spring-xd-jms-sender-1.0-SNAPSHOT.jar \
   com.cleverfishsoftware.spring.xd.jms.sender.RunMessageSenderArgs \
-  $broker_url $queue_name $message_rate $console_output
+  $broker_url $queue_name $message_rate $console_output"
+
+cmd="java -cp .:target/spring-xd-jms-sender-1.0-SNAPSHOT.jar \
+com.cleverfishsoftware.spring.xd.jms.sender.sample.RunMessageSender \
+$broker_url \
+$queue_name \
+$connectionFactoryClassName \
+$message_rate \
+$message_limit \
+$number_of_threads \
+$console_output"
+
+echo $cmd
+eval $cmd
 
 echo "JMS MessageSender started on PID $!"
 cd $pwd

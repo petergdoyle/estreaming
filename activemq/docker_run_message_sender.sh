872260569ca41b0e8ce21bdbf9fa71f939df737e
petergdoyle <peter.g.doyle@gmail.com>
2016-03-18 05:32:11 -0600

    fixed up the runtime scripts

--- activemq/docker_run_message_sender.sh
+++ activemq/docker_run_message_sender.sh
@@ -4,15 +4,13 @@
 
 img_name='estreaming/activemq'
 
-./RunMessageSenderArgs.sh
-
 read -e -p "Enter the number of instances to run: " -i "1" instances
+
+./RunMessageSender.sh
 
 for i in $(eval echo "{1..$instances"});   do
   container_name='estreaming_activemq_message_sender'_$i
-  start_cmd="java -cp .:./spring-xd-jms-sender-1.0-SNAPSHOT.jar \
-  com.cleverfishsoftware.spring.xd.jms.sender.RunMessageSenderArgs \
-  $broker_url $queue_name $message_rate $console_output"
+  start_cmd="$cmd"
   network="$network_native"
   docker_run
 done
---
+++ activemq/docker_run_simple_message_broker.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+. ../scripts/lib/docker_functions.sh
+. ../scripts/lib/network_functions.sh
+
+
+img_name='estreaming/activemq'
+
+container_name='estreaming_activemq_broker'
+start_cmd="java \
+-cp .:./spring-xd-jms-sender-1.0-SNAPSHOT.jar \
+com.cleverfishsoftware.spring.xd.jms.broker.RunBroker"
+network_port_mapped="-p 0.0.0.0:61616:61616 \
+-p 0.0.0.0:8161:8161 \
+-h activemq.dkr"
+network="$network_port_mapped"
+docker_run 8161,61616

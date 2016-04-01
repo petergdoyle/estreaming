be2fae767ff8662764910613a449e7426a0e8667
vagrant <vagrant@estreaming.vbx>
2016-03-31 23:15:36 -0400

    fixed a couple scripts

--- mq/docker_exec_check_queue.sh
+++ mq/docker_exec_check_queue.sh
@@ -4,7 +4,6 @@ read -e -p "Enter the Queue Manager name: " -i "QM1" qm
 
 read -e -p "Enter the Queue name: " -i "QUEUE1" qu
 
-docker exec \
-  -ti \
+docker exec -ti \
   estreaming_ibm_mq8_broker \
-  echo "DIS QL($qu) CURDEPTH" |runmqsc $qm
+  /bin/sh -c "echo \"DIS QL($qu) CURDEPTH\" |runmqsc $qm"
--- mq/docker_exec_copy_logs_to_host.sh
+++ mq/docker_exec_copy_logs_to_host.sh
@@ -1,10 +1,12 @@
 
 #!/bin/sh
 
+read -e -p "Enter the Queue Manager name: " -i "QM1" qm
+
 sudo mkdir -p logs/var/mqm/errors
 docker exec -ti estreaming_ibm_mq8_broker find /var/mqm/errors -name AMQERR01.LOG -type f -exec cp -fv {} /docker/logs/var/mqm/errors \;
 
-sudo mkdir -p logs/var/mqm/qmgrs/QM1/errors/
-docker exec -ti estreaming_ibm_mq8_broker find /var/mqm/qmgrs/QM1/errors/ -name AMQERR01.LOG -type f -exec cp -fv {} /docker/logs/var/mqm/qmgrs/QM1/errors \;
+sudo mkdir -p logs/var/mqm/qmgrs/$qm/errors/
+docker exec -ti estreaming_ibm_mq8_broker find /var/mqm/qmgrs/$qm/errors/ -name AMQERR01.LOG -type f -exec cp -fv {} /docker/logs/var/mqm/qmgrs/$qm/errors \;
 
 tree logs

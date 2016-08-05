782ec5b4949c134e9cc1b15a74ba3d6f2c09d28f
Peter Doyle <peter@Peters-MBP.hsd1.co.comcast.net>
2016-08-04 22:39:34 -0600

    having to explictly use advertised.host.name=127.0.0.1 for some clients

--- kafka/singlenode/docker_create_kafka_topic.sh
+++ kafka/singlenode/docker_create_kafka_topic.sh
@@ -5,4 +5,6 @@ read -e -p "Enter the zk host/port: " -i "localhost:2181" zk_host_port
 read -e -p "Enter replcation factor: " -i "1" replication_factor
 read -e -p "Enter number of partitions: " -i "1" partitions
 
-docker exec -ti estreaming_kafka_broker bin/kafka-topics.sh --create --zookeeper $zk_host_port --replication-factor $replication_factor --partitions $partitions --topic $topic
+cmd="docker exec -ti estreaming_kafka_broker bin/kafka-topics.sh --create --zookeeper $zk_host_port --replication-factor $replication_factor --partitions $partitions --topic $topic"
+echo "$cmd"
+eval "$cmd"

856aab4df8033637825e72bb2d6e02f99e0d592f
Peter Doyle <peter@Peters-iMac.hsd1.co.comcast.net>
2016-10-05 18:42:39 -0600

    remove old server.properties references

--- kafka/singlenode/docker_run_broker_multi.sh
+++ kafka/singlenode/docker_run_broker_multi.sh
@@ -5,9 +5,6 @@ cd $(dirname $0)
 
 
 img_name='estreaming/kafka'
-container_name='estreaming_kafka_broker'
-
-start_cmd='bin/kafka-server-start.sh config/server.properties'
 
 read -e -p "Run native networking mode(y/n): " -i "y" native
 if [[ "$native" == "n" || "$native" == "N" ]]; then

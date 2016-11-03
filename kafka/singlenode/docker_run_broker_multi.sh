9f120d8293847b787deffa796ef64418e1f792c3
petergdoyle <peter.g.doyle@gmail.com>
2016-11-03 11:59:41 -0600

    added latest back in that failed on merge

---
+++ kafka/singlenode/docker_run_broker_multi.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+cd $(dirname $0)
+. ../../scripts/lib/docker_functions.sh
+. ../../scripts/lib/network_functions.sh
+
+
+img_name='estreaming/kafka'
+<<<<<<< HEAD
+container_name='estreaming-kafka-broker'
+
+start_cmd='bin/kafka-server-start.sh config/server.properties'
+=======
+>>>>>>> fc6debfe5ea9f8bc583bed507298f73aa1947251
+
+read -e -p "Run native networking mode(y/n): " -i "y" native
+if [[ "$native" == "n" || "$native" == "N" ]]; then
+  read -e -p "Enter the network port number: " -i "9092" port
+  network="-p 0.0.0.0:$port"
+  links="-link estreaming-kafka-zk"
+else
+  network="$network_native"
+fi
+
+volumes="-v $PWD/shared:/shared"
+network="$network"
+
+read -e -p "Enter the number of brokers to run: " -i "1" instances
+port=9092
+for i in $(eval echo "{1..$instances"});   do
+  container_name='estreaming-kafka-broker'_$i
+  start_cmd="bin/kafka-server-start.sh config/server$i.properties"
+  docker_run "$port"
+  port=$((port+1))
+done

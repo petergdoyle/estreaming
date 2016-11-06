df82481f2997baa0d239dfe29f1b846a4e406314
petergdoyle <peter.g.doyle@gmail.com>
2016-11-06 03:04:42 -0700

    fixed up the console consumer to select a broker for multi-broker clusters

--- kafka/singlenode/docker_start_console_consumer.sh
+++ kafka/singlenode/docker_start_console_consumer.sh
@@ -5,14 +5,11 @@ read -e -p "Enter the zk host/port: " -i "localhost:2181" zk_host_port
 read -e -p "Enter the offset: " -i "--from-beginning" offset
 read -e -p "Enter the consumer group id: " -i "consumer-group-1" consumer_group
 
-echo "group.id=$consumer_group" > shared/$consumer_group.properties
-
-
 read -e -p "Run new kafka consumer: " -i "y" new_consumer
 
 if [[ "$new_consumer" == "n" || "$new_consumer" == "N" ]]; then
 
-  docker exec -ti estreaming-kafka-broker bin/kafka-console-consumer.sh \
+  docker exec -ti streamworks-kafka-broker bin/kafka-console-consumer.sh \
   --consumer.config /shared/$consumer_group.properties \
   --zookeeper $zk_host_port \
   --topic $topic \
@@ -22,9 +19,13 @@ else
 
   read -e -p "Enter the bootstrap server: " -i "localhost:9092" bootstrap_server
 
-  docker exec -ti estreaming-kafka-broker bin/kafka-console-consumer.sh \
+  brokers=$(docker ps |grep kafka-broker| awk '{print $NF}')
+  echo -e "The following Kafka brokers were found: \n$brokers"
+  brokers_arr=($brokers)
+  read -e -p "Pick a broker instance (any one):" -i ${brokers_arr[0]} selected_broker
+
+  docker exec -ti $selected_broker bin/kafka-console-consumer.sh \
   --new-consumer \
-  --consumer.config /shared/$consumer_group.properties \
   --bootstrap-server $bootstrap_server \
   --zookeeper $zk_host_port \
   --topic $topic \
